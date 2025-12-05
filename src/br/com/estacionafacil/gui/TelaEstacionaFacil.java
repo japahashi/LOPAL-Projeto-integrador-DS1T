4452d5477127297588495101dd2365e1d5141a6f
ISRAEL FUJIMOTO <25203688@dsn127m1247443.cfp127aluno.local>
2025-12-05 16:31:04 -0300

    Adcionado botões

--- src/br/com/estacionafacil/gui/TelaEstacionaFacil.java
+++ src/br/com/estacionafacil/gui/TelaEstacionaFacil.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,16 +19,13 @@ public class TelaEstacionaFacil extends Application {
 
         VBox root = new VBox();
         Scene scene = new Scene(root);
-
         root.setStyle("-fx-background-color: white;");
 
+        stage.setTitle("Estaciona Fácil");
         stage.setWidth(1030);
         stage.setHeight(600);
 
         VBox header = new VBox();
-
-        stage.setTitle("Estaciona Fácil");
-
         header.setStyle("-fx-background-color: #FCA311;");
         header.setPrefHeight(85);
 
@@ -41,17 +40,29 @@ public class TelaEstacionaFacil extends Application {
         carrosEstacionados.setPadding(new Insets(60, 300, 0,300));
 
         Button entrada = new Button("Nova entrada");
+        Button saida = new Button("Registrar saida");
+
+        HBox boxButtons = new HBox();
+        boxButtons.setSpacing(50);
+        boxButtons.setPadding(new Insets(30, 0, 0, 400));
+
+        Pane panebuttons = new Pane();
+        panebuttons.setPadding(new Insets(0, 0, 0, 0));
 
 
 
 
 
 
-
-
+        panebuttons.getChildren().add(boxButtons);
+        boxButtons.getChildren().addAll(entrada,saida);
         header.getChildren().addAll(labelTitulo, labelSubtitulo);
+
         root.getChildren().add(header);
         root.getChildren().add(carrosEstacionados);
+        root.getChildren().add(panebuttons);
+        root.getChildren().add(boxButtons);
+
 
         stage.setScene(scene);
         stage.show();
