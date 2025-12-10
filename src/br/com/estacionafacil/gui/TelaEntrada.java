d754689934641051f78653e3f04225784c08b027
ISRAEL FUJIMOTO <25203688@dsn127m1247443.cfp127aluno.local>
2025-12-10 14:58:32 -0300

    Atulizando as telas de entrada e saida

--- src/br/com/estacionafacil/gui/TelaEntrada.java
+++ src/br/com/estacionafacil/gui/TelaEntrada.java
@@ -1,8 +1,13 @@
 package br.com.estacionafacil.gui;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,16 +21,40 @@ public class TelaEntrada extends Stage {
         VBox header = new VBox();
         header.setStyle("-fx-background-color: #FCA311;");
         header.setPrefHeight(85);
-        Label labelTitulo = new Label("Controle de saída");
+        Label labelTitulo = new Label("Controle de entrada");
         labelTitulo.setStyle("-fx-text-fill: black;-fx-font-size: 25;");
         labelTitulo.setPadding(new Insets(25, 0, 0,40));
         header.getChildren().add(labelTitulo);
 
-        
+        GridPane gridFormulario = new GridPane();
+        gridFormulario.setVgap(20);
+        gridFormulario.setHgap(10);
+        gridFormulario.setPadding(new Insets(150, 0, 10, 30));
+
+        Label labelPlaca = new Label("Placa do veìculo");
+        TextField textFieldPlaca = new TextField();
+        Label labelModeloCarro = new Label("Modelo do carro");
+        TextField textFieldModeloCarro = new TextField();
+        Label labelNomeProprietario = new Label("Nome do proprietario");
+        TextField textFieldNomeProprietario = new TextField();
+
+        gridFormulario.add(labelPlaca, 0, 0);
+        gridFormulario.add(textFieldPlaca, 1, 0);
+        gridFormulario.add(labelModeloCarro, 0, 1);
+        gridFormulario.add(textFieldModeloCarro, 1, 1);
+        gridFormulario.add(labelNomeProprietario, 0, 2);
+        gridFormulario.add(textFieldNomeProprietario, 1, 2);
+
+        Button botaoConfirmarEntrada = new Button("Confirmar entrada");
+        botaoConfirmarEntrada.setStyle("-fx-background-color: #FCA311; -fx-text-fill: black;");
+        botaoConfirmarEntrada.setPrefHeight(70);
+        botaoConfirmarEntrada.setPrefWidth(170);
+
 
 
         root.getChildren().add(header);
-
+        root.getChildren().add(gridFormulario);
+        root.getChildren().add(botaoConfirmarEntrada);
 
         return new Scene(root, 1030, 600);
 
--- src/br/com/estacionafacil/gui/TelaSaida.java
+++ src/br/com/estacionafacil/gui/TelaSaida.java
@@ -1,6 +1,8 @@
 package br.com.estacionafacil.gui;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,6 +15,10 @@ public class TelaSaida extends Stage {
         VBox header = new VBox();
         header.setStyle("-fx-background-color: #FCA311;");
         header.setPrefHeight(85);
+        Label labelTitulo = new Label("Controle de saída");
+        labelTitulo.setStyle("-fx-text-fill: black;-fx-font-size: 25;");
+        labelTitulo.setPadding(new Insets(25, 0, 0,40));
+        header.getChildren().add(labelTitulo);
 
 
         root.getChildren().add(header);
